4bd0b879a3890c5de36430187df23912340e6155
bradjjc <jichangjung@yahoo.com>
2023-12-17 04:04:23 +0900

    기본 데이터 공유

--- routes/admin.js
+++ routes/admin.js
@@ -6,6 +6,8 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const products = [];
+
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
     // __dirname 우리 운영체제의 절대 경로를 이 프로젝트 폴더로 고정해주는 변수 __dirname route 폴더를 가르킴
@@ -14,8 +16,9 @@ router.get('/add-product', (req, res, next) => {
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res) => {
-    console.log(req.body['title']);
+    products.push({ title: req.body.title });
     res.redirect('/');
 });
 
-module.exports = router;
+exports.router = router;
+exports.products = products
