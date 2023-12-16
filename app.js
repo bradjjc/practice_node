400c416a60102fa6e70f66679e45aa5a3e412e1f
bradjjc <jichangjung@yahoo.com>
2023-12-16 17:34:58 +0900

    http 요청 실행 재한

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use('/add-product', (req, res, next) => {
     res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
-app.use('/product', (req, res) => {
+app.post('/product', (req, res) => {
     console.log(req.body['title']);
     res.redirect('/');
 });
