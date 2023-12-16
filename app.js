2cf8920bbab080107ce2a356bd7fba0f9e5fdf54
bradjjc <jichangjung@yahoo.com>
2023-12-16 16:54:41 +0900

    express 라우팅 설정

--- app.js
+++ app.js
@@ -3,13 +3,12 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware!');
-    // Alows the reauest to contiue to the next middleware in line
-    next();
+app.use('/app-product', (req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>The "Add Product" Page </h1>');
 });
 
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
     console.log('In another middleware!');
     res.send('<h1>Hello from Express</h1>');
 });
