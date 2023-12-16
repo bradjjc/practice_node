9c769923fe2da145400d9d53992468dafc350878
bradjjc <jichangjung@yahoo.com>
2023-12-16 19:29:07 +0900

    HTML 페이지 리턴

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,13 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+    // __dirname 우리 운영체제의 절대 경로를 이 프로젝트 폴더로 고정해주는 변수 __dirname route 폴더를 가르킴
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
 // /admin/add-product => POST
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,12 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.send('<h1>Hello from Express!</h1>');
+    // __dirname 우리 운영체제의 절대 경로를 이 프로젝트 폴더로 고정해주는 변수 __dirname route 폴더를 가르킴
+    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
 module.exports = router;
