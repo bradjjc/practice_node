d1f314a2db3540fc38db88d224cf030454f635d0
bradjjc <jichangjung@yahoo.com>
2023-12-16 17:32:42 +0900

    request body 읽기 body-parser

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 
 const express = require('express');
 
+const bodyParser = require('body-parser');
+
 const app = express();
 
-app.use('/app-product', (req, res, next) => {
-    console.log('In another middleware!');
-    res.send('<h1>The "Add Product" Page </h1>');
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+app.use('/product', (req, res) => {
+    console.log(req.body['title']);
+    res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
-    console.log('In another middleware!');
-    res.send('<h1>Hello from Express</h1>');
+    res.send('<h1>Hello from Express!</h1>');
 });
 
 app.listen(3000);
