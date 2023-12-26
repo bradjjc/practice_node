4ad7693b17dae464794ae02f4c6961989dceeed3
bradjjc <jichangjung@yahoo.com>
2023-12-26 21:44:18 +0900

    file system

--- controllers/products.js
+++ controllers/products.js
@@ -1,32 +1,30 @@
-
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-    // __dirname 우리 운영체제의 절대 경로를 이 프로젝트 폴더로 고정해주는 변수 __dirname route 폴더를 가르킴
-    // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-    res.render('add-product', {
-        pageTitle: 'Shop', 
-        path: '/admin/add-product', 
-        activeAddProduct: true, 
-        formsCSS: true,
-        productCSS: true,
-    });
-}
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
+};
 
-exports.postAddProduct = (req, res) => {
-    const product = new Product(req.body.title);
-    product.save();
-    res.redirect('/');
-}
+exports.postAddProduct = (req, res, next) => {
+  const product = new Product(req.body.title);
+  product.save();
+  res.redirect('/');
+};
 
 exports.getProducts = (req, res, next) => {
-    const products = Product.fetchAll();
+  Product.fetchAll(products => {
     res.render('shop', {
-        prods: products, 
-        pageTitle: 'Add Product', 
-        path: '/', 
-        hasProducts : products.length > 0,
-        activeShop: true,
-        productCSS: true,
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true
     });
-}
+  });
+};
