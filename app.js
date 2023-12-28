024681df3cf07945a47605d8e4d8d0368364c1ac
bradjjc <jichangjung@yahoo.com>
2023-12-29 01:06:41 +0900

    파일 스토리지 리팩토링

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-// const expressHbs = require('express-handlebars');
 
 const errorController = require('./controllers/error');
 
@@ -14,7 +13,7 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
