4ed7524aa63e57e7e973d721721f084c530158cd
bradjjc <jichangjung@yahoo.com>
2024-01-16 21:38:14 +0900

    sequelize connection setup

--- util/database.js
+++ util/database.js
@@ -1,10 +1,10 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'Bbangtto@2285'
-});
+const sequelize = new Sequelize(
+    'node-complete', 'root', 'Bbangtto@2285', 
+    {
+        dialect: 'mysql',
+        host: 'localhost',
+    });
 
-module.exports = pool.promise();
+module.exports = sequelize;
