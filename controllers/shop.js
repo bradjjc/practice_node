ab2a28fac1de403d07703fbd4f020d2376bae946
bradjjc <jichangjung@yahoo.com>
2024-01-28 02:29:10 +0900

    장바구니 추가 밑 검색 기능

--- controllers/shop.js
+++ controllers/shop.js
@@ -66,6 +66,7 @@ exports.getCart = (req, res, next) => {
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
   let fetchedCart;
+  let newQuantity = 1;
   req.user.getCart()
     .then(cart => {
       fetchedCart = cart;
@@ -75,17 +76,17 @@ exports.postCart = (req, res, next) => {
       if(products.length > 0) {
         product = products[0];
       }
-      let newQuantity = 1;
+      // 카드에 들어있을때 수량 추가
       if(product) {
-        //...
+        const oldQuantity = product.cartItem.quantity;
+        newQuantity = oldQuantity + 1;
+        return product;
       }
       // 카드에 들어있지 않을때 카트에 추가
       return Product.findByPk(prodId)
-        .then(product => {
-          return fetchedCart.addProduct(product, {through: {quantity: newQuantity}});
-        })
-          .catch(err => console.log(err));
-
+    })
+    .then(product => {
+      return fetchedCart.addProduct(product, {through: {quantity: newQuantity}});
     })
     .then(() => {
       res.redirect('/cart');
